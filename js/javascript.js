86348cf6e0a2ea87dfbefd59cf2cb92a5e2ffe9d
kevin Wairi <kevinwairi@gmail.com>
2023-03-16 21:21:18 +0300

    added a js file with simple code to toggle div color

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+function changeColor(value){
+    document.body.style.backgroundColor=value;
+}
+
+function clicked(){
+document.getElementById("changeColor").style.background="black";
+document.getElementById("changeColor").style.color="white";
+}
